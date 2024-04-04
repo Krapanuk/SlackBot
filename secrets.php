9642d6e7f35e7eaf2b065081a7544bef48deaea4
Krapanuk <46630690+Krapanuk@users.noreply.github.com>
2024-04-04 12:59:38 +0200

    Create secrets.php

---
+++ secrets.php
@@ -0,0 +1,7 @@
+<?php
+$api_key = "sk- ..."; // Your OpenAI API key
+$clientId = '...'; //In your App: Settings > Basic Information > App Credentials
+$clientSecret = '...'; //Your App: Settings > Basic Information > App Credentials
+$slackToken = 'xoxb-...'; //Your App: Features > Basic Information > App Credentials
+$verificationToken = '...'; //Your App: Settings > Basic Information > App Credentials
+?>
